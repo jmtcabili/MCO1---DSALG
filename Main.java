566a16d164d69d1f38c83c45a09174a0a4d6528b
Johan Marlo Cabili <jmtcabili@gmail.com>
2023-06-04 20:06:58 +0800

    uploaded files

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Use this method to run your experiments.
+    }
+}
---
+++ SortingAlgorithms.java
@@ -0,0 +1,34 @@
+/* This file contains implementations of sorting algorithms.
+ * You are NOT allowed to modify any of the given method headers.
+ */
+
+public class SortingAlgorithms {
+
+    /*
+     * You may define additional helper functions here if you need to.
+     * Make sure the helper functions have the private access modifier, as
+     * they will only be used in this class.
+     */
+
+    public void insertionSort(Record[] arr, int n) {
+        // TODO: Implement this sorting algorithm here.
+
+    }
+
+    public void selectionSort(Record[] arr, int n) {
+        // TODO: Implement this sorting algorithm here.
+
+    }
+
+    public void mergeSort(Record[] arr, int p, int r) {
+        // TODO: Implement this sorting algorithm here.
+
+    }
+
+    /*
+     * Define AT LEAST ONE more sorting algorithm here, apart from the
+     * ones given above. Make sure that the method accepts an array of
+     * records
+     */
+
+}
