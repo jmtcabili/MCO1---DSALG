dbab077a6c6ffa09a7bffeba29d5bd4b186d5627
Johan Marlo Cabili <jmtcabili@gmail.com>
2023-06-12 15:53:48 +0800

    Added counting sort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,7 +1,6 @@
 /* This file contains implementations of sorting algorithms.
  * You are NOT allowed to modify any of the given method headers.
  */
-
 public class SortingAlgorithms {
 
     /*
@@ -30,5 +29,39 @@ public class SortingAlgorithms {
      * ones given above. Make sure that the method accepts an array of
      * records
      */
+    public void countingSort(Record[] arr, int n){
+        
+        Record[] arrSorted = new Record[n+1];
 
+        //finding largest element
+        int i;
+        int max = 0;
+        for (i = 1; i < n; i++)
+            if (arr[i].getIdNumber() > arr[max].getIdNumber())
+                max = i;
+
+        //counting occurences of id numbers
+        int[] arrOccurence = new int[arr[max].getIdNumber()+1];
+        for (i = 0; i < n; i++)
+            arrOccurence[arr[i].getIdNumber()]+=1;
+
+        //setting up cumulative count
+        for (i = 1; i < max; i++)
+            arrOccurence[i] += arrOccurence[i-1];
+        
+        for (i = n-1; i >= 0; i++){
+            arrSorted[arrOccurence[arr[i].getIdNumber()]-1] = arr[i];
+            arrOccurence[arr[i].getIdNumber()]--; 
+        }
+        for(i = 0; i < n; i++){
+            arr[i] = arrSorted[i];
+        }
+        
+        
+
+
+
+
+        
+    }
 }
