1c0c3acbbee450d4ae5af8e6806be69dd0b92a9f
Johan Marlo Cabili <jmtcabili@gmail.com>
2023-06-21 13:57:17 +0800

    finalized main and created file writer to test records

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.io.FileWriter;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,10 +33,9 @@ public class Main {
             //set record
             if (choice != 0)
             {
-
                 for (int i = 1; i <= 7; i++){
                     Record[] record = null; 
-                    //change depending kung saan nakastore yung local repo 
+                   //change depending kung saan nakastore yung local repo 
                     folderLoc = "C:/Users/Johan/Documents/JOHAN/DLSU - College/3rd Semester AY 2022-2023/CCDSALG/MCO1---DSALG";
                     fileToSort = switch (i) {
                         case 1 -> "/data/almostsorted.txt";
@@ -47,8 +48,14 @@ public class Main {
                         default -> null;
                     };
                     record = fp.readFile(folderLoc+fileToSort);
-
+                    System.out.print("(" + record.length + ") ");
                     
+                    /* Plug this inside one of the cases below to test if ur algo works with a certain file
+                    *  if (i == 1){ //change i depending on file you wanna test
+                            for(int j = 0; j < record.length; j++)
+                                System.out.println(record[j].getIdNumber());
+                        }
+                    */
                     switch(choice){
                         case 1: 
                             startTime = System.currentTimeMillis();
@@ -62,18 +69,29 @@ public class Main {
                             break;
                         case 3: 
                             startTime = System.currentTimeMillis();
-                            //algorithm.mergeSort(record, //p, //r);
+                            algorithm.mergeSort(record, 0, record.length-2);
                             endTime = System.currentTimeMillis();
                             break;
                         case 4: 
                             startTime = System.currentTimeMillis();
                             algorithm.countingSort(record, record.length);
-                            endTime = System.currentTimeMillis();
+                            endTime = System.currentTimeMillis();   
                             break;
                     }
-                    
+
+                    try {
+                        FileWriter writer = new FileWriter(fileToSort.substring(6,fileToSort.length()-4)+"-result.txt");
+                        writer.write("");
+                        for (int k = 0; k < record.length; ++k)
+                            writer.write(record[k].getIdNumber() + " " + record[k].getName() + "\n");
+                        writer.close();
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        e.printStackTrace();
+                    }
+                    System.out.println(record[record.length-1].getIdNumber() + " " + record[record.length-1].getName());
                     long executionTime = endTime - startTime;
-                    System.out.println(fileToSort + " : " + executionTime + "(ms)");
+                    System.out.println(fileToSort.substring(6) + " : " + executionTime + "(ms)\n");
                     
                 
                 }
@@ -85,4 +103,3 @@ public class Main {
 
     }
 }
-       
