090bedf3bc1c638a7c55f547905ab957286d9b36
Johan Marlo Cabili <jmtcabili@gmail.com>
2023-06-26 01:32:23 +0800

    a;sldjf;lsadkfj;alksdjf

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -10,88 +10,115 @@ public class SortingAlgorithms {
      */
 
     public void insertionSort(Record[] arr, int n) {
+
+        long ctr = 0; 
         for (int i = 1; i < n; ++i) {
-            Record key = arr[i];
-            int j = i - 1;
+            Record key = arr[i]; ctr+=1;
+            int j = i - 1; ctr++;
 
             while (j >= 0 && arr[j].getIdNumber() > key.getIdNumber()) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+                arr[j + 1] = arr[j]; ctr++;
+                j = j - 1; ctr++;
             }
-            arr[j + 1] = key;
+            arr[j + 1] = key; ctr++;
         }
+        System.out.println(ctr);
 
     }
 
     public void selectionSort(Record[] arr, int n) {
-        
+
+        long ctr = 0;
+
         for (int j = 0; j < n-1 ; j++){
-            int minIndex = j;
+            int minIndex = j; ctr++;
             for (int k = j+ 1; k < n; k++){ 
                 if(arr[minIndex].getIdNumber() > arr[k].getIdNumber())
-                    minIndex = k;
+                    minIndex = k; ctr++;
             }
-            if (minIndex != j){
-                Record temp = arr[j];
-                arr[j] = arr[minIndex];
-                arr[minIndex] = temp;
+            if (minIndex != j){ 
+                Record temp = arr[j]; ctr++;
+                arr[j] = arr[minIndex]; ctr++;
+                arr[minIndex] = temp; ctr++;
             }
         }
+        System.out.println(ctr);
     }
 
-    private void merge(Record[] arr, int p, int q, int r) {
-        int sizeL = q - p + 1, 
+    private long merge(Record[] arr, int p, int q, int r) {
+        long ctr = 0; 
+        int sizeL = q - p + 1,
             sizeR = r - q;
-        Record[] arrL = new Record[sizeL];
-        Record[] arrR = new Record[sizeR];
+        ctr+=2; 
+        Record[] arrL = new Record[sizeL]; ctr++;
+        Record[] arrR = new Record[sizeR]; ctr++;
 
         // copies the 2 halves of the array into their respective sub-arrays
-        for(int i = 0; i < sizeL; i++)
-            arrL[i] = arr[p+i];
-        for(int i = 0; i < sizeR; i++)
+        for(int i = 0; i < sizeL; i++){
+            arrL[i] = arr[p+i]; 
+            ctr++;
+        }
+        for(int i = 0; i < sizeR; i++){
             arrR[i] = arr[q + 1 + i];
+            ctr++;
+        }
         
         // re-initializes the indices of arrL[], arrR[], and arr[]
-        int i = 0, j = 0, k = p;
+        int i = 0, j = 0, k = p; ctr+=3;
 
         // merges the sub-arrays
         while(i < sizeL && j < sizeR){
             if(arrL[i].getIdNumber() <= arrR[j].getIdNumber()) {
-                arr[k] = arrL[i];
-                i++;
+                arr[k] = arrL[i]; ctr++; 
+                i++; ctr++;
             }
             else {
-                arr[k] = arrR[j];
-                j++;
+                arr[k] = arrR[j]; ctr++;
+                j++;ctr++;
             }
-            k++;
+            k++; ctr++;
         }
 
         // the remaining elements from arrL[] and arrR[] are put in arr[]
         while(i < sizeL) {
-            arr[k] = arrL[i];
-            i++;
-            k++;
+            arr[k] = arrL[i]; ctr++;
+            i++; ctr++;
+            k++; ctr++;
         }
         while(j < sizeR) {
-            arr[k] = arrR[j];
-            j++;
-            k++;
+            arr[k] = arrR[j]; ctr++;
+            j++; ctr++;
+            k++; ctr++;
         }
+        return ctr; 
     }
 
-    public void mergeSort(Record[] arr, int p, int r) {
-        int q;
+    public long mergeSort(Record[] arr, int p, int r) {
+        long ctr = 0; 
+        int q; ctr++; 
         if(p < r){
             q = (p + r)/2; // gets the midpoint index of the array
-
+            ctr++;
             // recursive call to the 2 halves of the record array
-            mergeSort(arr, p, q);
-            mergeSort(arr, q+1, r);
+            ctr += mergeSort(arr, p, q);
+            ctr += mergeSort(arr, q+1, r);
 
             // merges the 2 halves
-            merge(arr, p, q, r);
+            ctr += merge(arr, p, q, r);
         }
+
+        /*
+        int j = 0; boolean isSorted = true; 
+        while (j < arr.length-1 && isSorted){
+                        if (arr[j].getIdNumber() > arr[j+1].getIdNumber())
+                            isSorted = false;
+                        j++;
+                    }
+        if (isSorted)
+            System.out.println(ctr);
+        */
+        
+        return ctr; 
     }
     /*
      * Define AT LEAST ONE more sorting algorithm here, apart from the
@@ -100,33 +127,42 @@ public class SortingAlgorithms {
      */
     public void countingSort(Record[] arr, int n){
         
-        Record[] arrSorted = new Record[n];
+        long ctr = 0; 
+        Record[] arrSorted = new Record[n+1]; ctr++;
 
         //finding largest element
-        int i;
-        int max = 0;
+        int i; ctr++;
+        int max = 0; ctr++;
         for (i = 0; i < n; i++)
-            if (arr[i].getIdNumber() > max)
-                max = arr[i].getIdNumber();
-
+            if (arr[i].getIdNumber() > max){
+                max = arr[i].getIdNumber(); ctr++;
+            }
+                
         //counting occurences of id numbers
-        int[] arrOccurence = new int[max+1];
-        for (i = 0; i < n; ++i)
-            arrOccurence[arr[i].getIdNumber()]+=1;
+        int[] arrOccurence = new int[max+1]; ctr++;
+        for (i = 0; i < n; ++i) {
+            arrOccurence[arr[i].getIdNumber()]+=1; 
+            ctr++;
+        }
+            
 
         //setting up cumulative count
-        for (i = 1; i <= max; i++)
-            arrOccurence[i] += arrOccurence[i-1];
+        for (i = 1; i <= max; i++){
+            arrOccurence[i] += arrOccurence[i-1]; ctr++;
+        }
+            
         
         //use count of number as placement for that number in sorted array
         for (i = n-1; i >= 0; i--){
-            arrSorted[arrOccurence[arr[i].getIdNumber()]-1] = arr[i];
-            arrOccurence[arr[i].getIdNumber()]--; 
+            arrSorted[arrOccurence[arr[i].getIdNumber()]-1] = arr[i]; ctr++;
+            arrOccurence[arr[i].getIdNumber()]--; ctr++;
         }
 
         //copy contents of sorted array to unsorted record
         for(i = 0; i < n; i++){
-            arr[i] = arrSorted[i];
+            arr[i] = arrSorted[i]; ctr++;
         }       
+
+        System.out.println(ctr);
     }
 }
